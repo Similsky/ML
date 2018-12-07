82ee20abcf09430899fea8cf089e012cb9123cca
Similsky <gkyqfz@163.com>
2018-12-07 17:35:16 +0800

    Create hello_world.py
    add sum of i, j

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# encoding=utf-8
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+for i in range(10):
+    for j in range(i):
+        print("i+j=", i+j)
